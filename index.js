11fe8893f8cb0ef9592a5180f234e8704a06b833
uthaya23 <113130843+uthaya23@users.noreply.github.com>
2024-09-01 21:14:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ const app=express()
 app.use(express.json())
 app.use(cors())
 app.use(cookieParser())
-app.use(cors());
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 dotenv.config()
 app.use('/auth',AdminRouter)
 app.use('/student', StudentRouter)
